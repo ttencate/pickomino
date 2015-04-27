11c5bc8712700bf1349a736d86dbb59512f96ef2
Thomas ten Cate <ttencate@gmail.com>
2015-04-27 18:09:36 +0200

    Split off maths into separate file

--- pickomino.cc
+++ pickomino.cc
@@ -1,4 +1,5 @@
 #include "dice.h"
+#include "maths.h"
 
 #include <algorithm>
 #include <array>
@@ -15,30 +16,6 @@ const int MAX_DIE_VALUE = 5;
 
 typedef double Probability;
 typedef double ExpectedWorms;
-
-int nChooseK(int n, int k) {
-  int out = 1;
-  for (int i = 1; i < k; ++i) {
-    out *= (n + 1 - i) / i;
-  }
-  return out;
-}
-
-long int factorial(int n) {
-  long int f = 1;
-  for (; n; --n) {
-    f *= n;
-  }
-  return f;
-}
-
-long int power(int n, int k) {
-  long int p = 1;
-  for (; k; --k) {
-    p *= n;
-  }
-  return p;
-}
 
 Probability rollProbability(Dice const &dice) {
   int n = dice.numDice();
